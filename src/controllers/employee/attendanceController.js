fc6ea23d2496d5759c465a17722fd3da39c87448
Anish Bag <anishbag18@gmail.com>
2026-07-24 11:29:33 +0530

    new

--- src/controllers/employee/attendanceController.js
+++ src/controllers/employee/attendanceController.js
@@ -1,6 +1,5 @@
-import Attendance from "../models/Attendance.js";
-import Employee from "../models/Employee.js";
-
+import Attendance from "../../models/Attendance.js";
+import Employee from "../../models/Employee.js";
 
 // Employee Check In
 export const checkIn = async (req, res) => {
@@ -18,11 +17,11 @@ export const checkIn = async (req, res) => {
       });
     }
 
-    // Today's Date (00:00:00)
+    // Ajker Date (00:00:00)
     const today = new Date();
     today.setHours(0, 0, 0, 0);
 
-    // Already Checked In?
+    // jodi age theke Checked In?
     const alreadyCheckedIn = await Attendance.findOne({
       employee: employee._id,
       date: today,
@@ -35,7 +34,7 @@ export const checkIn = async (req, res) => {
       });
     }
 
-    // Create Attendance
+    //  Attendance create hobe
     const attendance = await Attendance.create({
       employee: employee._id,
       date: today,
@@ -55,3 +54,74 @@ export const checkIn = async (req, res) => {
     });
   }
 };
+
+
+
+// Employee Check Out
+export const checkOut = async (req, res) => {
+  try {
+    // Find Employee
+    const employee = await Employee.findOne({
+      userId: req.user._id,
+      isDeleted: false,
+    });
+
+    if (!employee) {
+      return res.status(404).json({
+        success: false,
+        message: "Employee profile not found",
+      });
+    }
+
+    // ajker date Date
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    // Find Today's Attendance
+    const attendance = await Attendance.findOne({
+      employee: employee._id,
+      date: today,
+    });
+
+    if (!attendance) {
+      return res.status(400).json({
+        success: false,
+        message: "Please check in first",
+      });
+    }
+
+    if (attendance.checkOut) {
+      return res.status(400).json({
+        success: false,
+        message: "You have already checked out today",
+      });
+    }
+
+    // Save Check Out Time
+    attendance.checkOut = new Date();
+
+    // Calculate Working Hours
+    const totalMilliseconds =
+      attendance.checkOut.getTime() - attendance.checkIn.getTime();
+
+    attendance.workingHours = Number(
+      (totalMilliseconds / (1000 * 60 * 60)).toFixed(2)
+    );
+
+    await attendance.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Check Out Successful",
+      attendance,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+
--- src/routes/employee/attendanceRoutes.js
+++ src/routes/employee/attendanceRoutes.js
@@ -1,6 +1,14 @@
 import express from "express";
-
+import protect from "../../middleware/authMiddleware.js";
+import authorize from "../../middleware/roleMiddleware.js";
+import { checkIn, checkOut } from "../../controllers/employee/attendanceController.js";
 const router = express.Router();
+
+
+router.post("/check-in",protect,authorize("employee"),checkIn);
+
+router.post("/check-out",protect,authorize("employee"),checkOut)
+
 
 export default router;
 
