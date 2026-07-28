43105090fe54514b83fa53c47641add02ab63c23
Anish Bag <anishbag18@gmail.com>
2026-07-28 14:29:15 +0530

    new21

--- src/controllers/admin/taskController.js
+++ src/controllers/admin/taskController.js
@@ -54,3 +54,63 @@ export const createTask = async (req, res) => {
     });
   }
 };
+
+
+export const getAllTasks = async (req, res) => {
+  try {
+    const {
+      page = 1,
+      limit = 10,
+      search = "",
+      status,
+      priority,
+    } = req.query;
+
+    const query = {
+      isDeleted: false,
+    };
+
+    if (search) {
+      query.title = {
+        $regex: search,
+        $options: "i",
+      };
+    }
+
+    if (status) {
+      query.status = status;
+    }
+
+    if (priority) {
+      query.priority = priority;
+    }
+
+    const totalTasks = await Task.countDocuments(query);
+
+    const tasks = await Task.find(query)
+      .populate({
+        path: "assignedTo",
+        select: "employeeId firstName lastName department designation",
+      })
+      .populate({
+        path: "assignedBy",
+        select: "name email",
+      })
+      .sort({ createdAt: -1 })
+      .skip((Number(page) - 1) * Number(limit))
+      .limit(Number(limit));
+
+    res.status(200).json({
+      success: true,
+      totalTasks,
+      currentPage: Number(page),
+      totalPages: Math.ceil(totalTasks / Number(limit)),
+      tasks,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- src/routes/admin/taskRoutes.js
+++ src/routes/admin/taskRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import protect from "../../middleware/authMiddleware.js";
 import authorize from "../../middleware/roleMiddleware.js";
-import { createTask } from "../../controllers/admin/taskController.js";
+import { createTask, getAllTasks } from "../../controllers/admin/taskController.js";
 
 const router = express.Router();
 
 router.post("/",protect,authorize("admin"),createTask);
-
+router.get("/",protect,authorize("admin"),getAllTasks);
 export default router;
